0f28e5f9af0c1e400a7b0bd1de4038bb8417534e
Marcobi81 <marco_bi@libero.it>
2022-11-25 16:30:14 +0100

    autopilot3

--- pong/main.lua
+++ pong/main.lua
@@ -258,7 +258,7 @@ function love.update(dt)
         player2.dy= - PADDLE_SPEED
     else
         player2.dy = 0
-
+    end
 
     -- update our ball based on its DX and DY only if we're in play state;
     -- scale the velocity by dt so movement is framerate-independent
